1e771b20264cc20515936e13338032bd12aa2fd8
chanyoungcoding <white1614@naver.com>
2025-05-11 22:35:03 +0900

    Controller test

--- src/main/java/com/coffee/myCoffee/controller/HomeController.java
+++ src/main/java/com/coffee/myCoffee/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.coffee.myCoffee.controller;
 
+import com.coffee.myCoffee.Entity.Grade;
+import com.coffee.myCoffee.Entity.Member;
+import com.coffee.myCoffee.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/coffee")
 public class HomeController {
+
+    private final MemberService memberService;
 
     @GetMapping("/home")
     public String home(Model model) {
@@ -16,8 +23,15 @@ public class HomeController {
     }
 
     @GetMapping("/user")
-    public String user() {
-        return "user";
+    public String user(Model model) {
+
+        Member member = new Member(1L, "memberTest-A", Grade.BASIC);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        model.addAttribute("member", findMember);
+
+        return "template/layout/user";
     }
 
     @GetMapping("/coffee")
